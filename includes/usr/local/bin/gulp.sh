6975fb2132f4b797d0bd070735433deecb14cabd
Thomas Schulze <tschulze@codemonauts.com>
2017-10-12 15:48:24 +0200

    add ngrok tunnel, add dotenv check

--- includes/usr/local/bin/gulp.sh
+++ includes/usr/local/bin/gulp.sh
@@ -10,12 +10,20 @@ if [ -e /local/composer.json ]; then
 	composer install
 fi
 
+if [ -e /local/.env.local ]; then
+	echo "Copying .env.local to .env"
+	cp /local/.env.local /local/.env
+fi
+
 if [ -d /local/src ]; then
+	echo "Running gulp from /src"
 	cd /local/src
 elif [ -d /local/resources ]; then
+	echo "Running gulp from /resources"
 	cd /local/resources
 else
 	echo "No valid directory found!"
+	exit
 fi
 
 npm install
