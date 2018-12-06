c1eb330b685d2f636e61085fecc0f3a7513d9f75
Felix Breidenstein <felix@codemonauts.com>
2018-12-06 17:07:04 +0100

    Added script to recognize sql dump compression before importing

---
+++ includes/usr/local/bin/extract.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+set -eu
+
+DATABASE=$1
+FILENAME=$2
+
+if [[ "$FILENAME" == *.sql ]]; then
+    mysql "$DATABASE" < /local/"$FILENAME"
+elif [[ "$FILENAME" == *.sql.gz ]]; then
+	zcat /local/"$FILENAME" | mysql "$DATABASE"
+elif [[ "$FILENAME" == *.sql.zst ]]; then
+	zstd -d -c /local/"$FILENAME" | mysql "$DATABASE"
+else
+	echo "Unknown file format"
+fi
+
