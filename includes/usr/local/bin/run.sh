c8f3836fc51063b67f6b85b9734d190641282518
Felix Breidenstein <felix@codemonauts.com>
2019-05-02 14:21:56 +0200

    Set correct php-cli version on startup

--- includes/usr/local/bin/run.sh
+++ includes/usr/local/bin/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Switch php version for the cli
+update-alternatives --set php /usr/bin/php"$PHPVERSION"
+
 service php"$PHPVERSION"-fpm start
 rm -rf /local/craft/storage/runtime
 /usr/sbin/nginx -c /etc/nginx/nginx-"$PHPVERSION".conf
