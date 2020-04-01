ae420bc173d25bf15e3f0a75c74eece75b492423
Felix Breidenstein <felix@codemonauts.com>
2020-04-01 17:14:45 +0200

    Support .zip for DB dumps. Fixes #7

--- includes/usr/local/bin/extract.sh
+++ includes/usr/local/bin/extract.sh
@@ -10,6 +10,8 @@ elif [[ "$FILENAME" == *.sql.gz ]]; then
 	zcat /local/"$FILENAME" | mysql "$DATABASE"
 elif [[ "$FILENAME" == *.sql.zst ]]; then
 	zstd -d -c /local/"$FILENAME" | mysql "$DATABASE"
+elif [[ "$FILENAME" == *.zip ]]; then
+	unzip -p "$FILENAME" | mysql "$DATABASE"
 else
 	echo "Unknown file format"
 fi
