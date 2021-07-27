a845e0202802b203e1ae5e564c6301b9b5703314
Felix Breidenstein <felix@codemonauts.com>
2021-07-27 15:46:32 +0200

    Only run chown when it's needed

--- includes/usr/local/bin/run.sh
+++ includes/usr/local/bin/run.sh
@@ -4,7 +4,11 @@ if [ ! -z "$CUSTOM_UID" ] && [ ! -z "$CUSTOM_GID" ]; then
     usermod -u $CUSTOM_UID www-data && groupmod -g $CUSTOM_GID www-data
 fi
 
-chown -R www-data:www-data /local/
+OWNER=`stat -c '%U' /local/config`
+if [ $OWNER != "www-data" ]; then
+    echo "Need to fix permissions of your project..."
+    chown -R www-data:www-data /local/
+fi
 
 # Switch php version for the cli
 sudo update-alternatives --set php /usr/bin/php"$PHPVERSION"
