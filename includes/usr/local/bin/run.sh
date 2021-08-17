54580fe4307f3d31261c74c4d53b73cb3d9c4fdd
Felix Breidenstein <felix@codemonauts.com>
2021-08-17 15:04:34 +0200

    Added some log output to run.sh

--- includes/usr/local/bin/run.sh
+++ includes/usr/local/bin/run.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
+#! /bin/bash
+set -eu
+
+echo "Starting craft container"
 
 if [ ! -z "$CUSTOM_UID" ] && [ ! -z "$CUSTOM_GID" ]; then
+    echo "Adding custom user to www-data group"
     usermod -u $CUSTOM_UID www-data && groupmod -g $CUSTOM_GID www-data
 fi
 
+echo "Checking if we need to chown the docroot"
 OWNER=`stat -c '%U' /local/config`
 if [ $OWNER != "www-data" ]; then
     echo "Need to fix permissions of your project..."
     chown -R www-data:www-data /local/
 fi
 
-# Switch php version for the cli
+echo "Switch php version for the cli"
 sudo update-alternatives --set php /usr/bin/php"$PHPVERSION"
 
+echo "Starting php-fpm$PHPVERSION"
 sudo service php"$PHPVERSION"-fpm start
+
+echo "Clearing /storage/runtime/"
 rm -rf /local/craft/storage/runtime
+
+echo "Starting nginx"
 sudo /usr/sbin/nginx -c /etc/nginx/nginx-"$PHPVERSION".conf
