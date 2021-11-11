253c6c19f6d11ee8a336c525c07bf27f153a893d
Felix Breidenstein <felix@codemonauts.com>
2021-11-11 15:19:45 +0100

    import: Check if file exists

--- includes/usr/local/bin/extract.sh
+++ includes/usr/local/bin/extract.sh
@@ -1,8 +1,19 @@
 #! /usr/bin/env bash
-set -eu
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+	echo "Usage: import <database name> <filename>"
+	exit 1
+fi
 
 DATABASE=$1
 FILENAME=$2
+
+if [[ ! -f "$FILENAME" ]]; then
+	echo "Error: Database dump was not found in the project folder"
+	exit 1
+fi
 
 if [[ "$FILENAME" == *.sql ]]; then
     mysql "$DATABASE" < /local/"$FILENAME"
